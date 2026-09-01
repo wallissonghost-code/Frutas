57bcbf3e71830f2aa1670859392a8d51bea83041
ghostzinho <wallissonghost@gmail.com>
2026-09-01 12:37:36 -0300

    Sobe Frutas para Beta 0.0.10

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 (()=>{'use strict';
 window.FRUTAS_CONFIG=Object.freeze({
   gameId:'frutas',
-  version:'Beta 0.0.9',
-  assetVersion:'0.0.9',
+  version:'Beta 0.0.10',
+  assetVersion:'0.0.10',
   gameStorageKey:'frutas-game-state-v1',
   finalMergeBonus:500,
   removePointsDefault:100,
