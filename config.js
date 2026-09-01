1e48a69617ca58f2f62e63aceb889cef82b35d11
ghostzinho <wallissonghost@gmail.com>
2026-09-01 19:37:03 -0300

    Bump Frutas to Beta 0.0.18

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 (()=>{'use strict';
 window.FRUTAS_CONFIG=Object.freeze({
   gameId:'frutas',
-  version:'Beta 0.0.17',
-  assetVersion:'0.0.17',
+  version:'Beta 0.0.18',
+  assetVersion:'0.0.18',
   gameStorageKey:'frutas-game-state-v1',
   finalMergeBonus:500,
   removePointsDefault:100,
