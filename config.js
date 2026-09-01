0941e24496b7d135fbc353668ef5708807aa2074
ghostzinho <wallissonghost@gmail.com>
2026-09-01 18:56:43 -0300

    Beta 0.0.17 add HUD visibility preference

--- config.js
+++ config.js
@@ -1,13 +1,14 @@
 (()=>{'use strict';
 window.FRUTAS_CONFIG=Object.freeze({
   gameId:'frutas',
-  version:'Beta 0.0.16',
-  assetVersion:'0.0.16',
+  version:'Beta 0.0.17',
+  assetVersion:'0.0.17',
   gameStorageKey:'frutas-game-state-v1',
   finalMergeBonus:500,
   removePointsDefault:100,
   panelStorageKey:'frutas-liveplus-token',
   panelCodeKey:'frutas-panel-code',
+  hudVisibleKey:'frutas-live-hud-visible',
   effectsStorageKey:'frutas-live-effects-v1'
 });
 })();
