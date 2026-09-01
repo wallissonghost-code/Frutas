58c71dec54cb33c7fdcb421c7013ac2ae5f8c2d5
ghostzinho <wallissonghost@gmail.com>
2026-09-01 07:52:51 -0300

    Centraliza configuracao do core

--- config.js
+++ config.js
@@ -3,6 +3,7 @@ window.FRUTAS_CONFIG=Object.freeze({
   gameId:'frutas',
   version:'Beta 0.0.8',
   assetVersion:'0.0.8',
+  gameStorageKey:'frutas-game-state-v1',
   finalMergeBonus:500,
   removePointsDefault:100,
   panelStorageKey:'frutas-liveplus-token',
