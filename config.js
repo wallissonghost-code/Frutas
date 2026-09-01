b6c3f60a2c9e103c0c4dcb14f17e268c07aff34a
ghostzinho <wallissonghost@gmail.com>
2026-09-01 07:03:36 -0300

    Centraliza configuracao do Frutas

---
+++ config.js
@@ -0,0 +1,12 @@
+(()=>{'use strict';
+window.FRUTAS_CONFIG=Object.freeze({
+  gameId:'frutas',
+  version:'Beta 0.0.8',
+  assetVersion:'0.0.8',
+  finalMergeBonus:500,
+  removePointsDefault:100,
+  panelStorageKey:'frutas-liveplus-token',
+  panelCodeKey:'frutas-panel-code',
+  effectsStorageKey:'frutas-live-effects-v1'
+});
+})();
