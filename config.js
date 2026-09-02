e4d524a50f6e91b5bb2d2ea3cb7647c404be75ef
ghostzinho <wallissonghost@gmail.com>
2026-09-01 21:18:12 -0300

    Bump Frutas config to Beta 0.0.19

--- config.js
+++ config.js
@@ -1,14 +1,16 @@
 (()=>{'use strict';
 window.FRUTAS_CONFIG=Object.freeze({
   gameId:'frutas',
-  version:'Beta 0.0.18',
-  assetVersion:'0.0.18',
+  version:'Beta 0.0.19',
+  assetVersion:'0.0.19',
   gameStorageKey:'frutas-game-state-v1',
   finalMergeBonus:500,
   removePointsDefault:100,
   panelStorageKey:'frutas-liveplus-token',
   panelCodeKey:'frutas-panel-code',
   hudVisibleKey:'frutas-live-hud-visible',
-  effectsStorageKey:'frutas-live-effects-v1'
+  effectsStorageKey:'frutas-live-effects-v1',
+  liveQueueKey:'frutas-live-queue-v1',
+  seenCommandsKey:'frutas-live-seen-v1'
 });
 })();
